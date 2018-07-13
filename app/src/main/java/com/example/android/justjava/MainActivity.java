5837636fbd4c551d499c73247ec4b05b03bc6d2d
Hales <hrymel@gmail.com>
2018-07-12 23:48:52 -0400

    added increment and decrement buttons and fixed defect with total

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,7 +1,7 @@
 /** in the project's AndroidManifest.xml file.
         **/
 
-        package com.example.android.justjava;
+package com.example.android.justjava;
 
 
 
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
+
 
 import com.example.android.justjava.R;
 
@@ -16,7 +18,7 @@ import com.example.android.justjava.R;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-
+int quantity = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int price = 5;
+        displayPrice(quantity * price);
     }
 
     /**
@@ -44,6 +47,25 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method is call when the + button is clicked.
+     * It increases quantity by 1.
+     */
+    public void increment(View view){
+        quantity ++;
+        display(quantity);
+
+    }
+
+    /**
+     * This method is call when the - button is clicked.
+     * It decreases quantity by 1.
+     */
+    public void decrement(View view){
+        quantity--;
+        display(quantity);
     }
 }
 
